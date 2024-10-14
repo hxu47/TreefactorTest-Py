5a6454f23334e1126d7139203fb4d7ccdac4dcc6
hxu47 <huidongxu1222@gmail.com>
2024-10-13 22:20:47 -0300

    after refactoring

--- parameterize_variable.py
+++ parameterize_variable.py
@@ -1,6 +1,5 @@
-def calculate_total():
-    tax_rate = 0.1  # Local variable
+def calculate_total(tax_rate):  # Now a parameter
     subtotal = 100
     return subtotal + (subtotal * tax_rate)
 
-print(calculate_total())
+print(calculate_total(0.1))
